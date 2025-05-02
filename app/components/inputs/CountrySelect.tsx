0f534fa824824243c7de2aa507da3a2ed8b5a154
Jahir1515 <166346937+Jahir1515@users.noreply.github.com>
2025-05-01 19:38:09 -0500

    Systemte creation of Airbnb

---
+++ app/components/inputs/CountrySelect.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import useCountries from '@/app/hooks/useCountries';
+import Select from 'react-select'
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+}
+
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange: (value: CountrySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({
+  value,
+  onChange
+}) => {
+  const { getAll } = useCountries()
+
+  return (
+    <div>
+      <Select
+        placeholder="Anywhere"
+        isClearable
+        options={getAll()}
+        value={value}
+        onChange={(value) => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div className="flex flex-row items-center gap-3">
+            <img src={option.flag} className="w-8 h-5 object-cover" />
+            <div>
+              {option.label},
+              <span className="text-neutral-500 ml-1">{option.region}</span>
+            </div>
+          </div>
+        )}
+        classNames={{
+          control: () => "p-3 border-2",
+          input: () => "text-lg",
+          option: () => "text-lg",
+        }}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 6,
+          colors: {
+            ...theme.colors,
+            primary: "black",
+            primary25: "#ffe4e6",
+          },
+        })}
+        menuPlacement="auto" 
+        menuPortalTarget={document.body} 
+        styles={{
+          menuPortal: (base) => ({ ...base, zIndex: 9999 }), 
+        }}
+      />
+    </div>
+  );
+}
+
+export default CountrySelect
